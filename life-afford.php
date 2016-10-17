02b4ceb84254dd575601b9a3ca2c21f9b61db41c
Chris Spurlock <cjs7w6@gmail.com>
2016-10-16 20:05:49 -0500

    Add last two child-level Life landing pages
    Add pages for “I Know What I Need” and “I Know What I Can Afford”

---
+++ life-afford.php
@@ -0,0 +1,59 @@
+<?php require_once('header.html'); ?>
+
+<div id="life_afford" class="background-wrap child-landing life-landing">
+	<section id="life_afford_hero" class="cover text-center">
+        <!-- <div class="overlay"></div> -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>SAVE UP TO 5O% ON YOUR PREMIUM.</h1>
+                    <h2>Life insurance is often significantly less expensive for healthy people like you.</h2>
+                </div>
+            </div>
+            <div class="row">
+            	<div class="col-md-12">
+            		<div class="form-wrapper">
+            			<div class="primary-container">
+            				<h4>I know how much I need</h4>
+            				<p>Experts recommened 8x your salary for optimal coverage.</p>
+            				<p>See our full <a href="calculator.php">Calculator</a>.</p>
+            				<form>
+            					<input type="text" placeholder="Amount">
+            					<span>/ month</span>
+            					<input type="submit" value="Go">
+            				</form>
+            			</div>
+            			<a href="#" class="secondary-container">
+            				<h5>Help Me Determine <br>My Coverage</h5>
+            			</a>
+            			<a href="#" class="secondary-container">
+            				<h5>I Know How <br>Much I Need</h5>
+            			</a>
+            		</div>
+            	</div>
+            </div>
+        </div>
+    </section>
+    <section id="life_help_tiles" class="tiles child-tiles">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-12">
+    				<a href="blood.php" id="blood" class="tile">
+    					<h3>Athletic Blood Profile</h3>
+    				</a>
+    				<a href="#" id="genome" class="tile">
+    					<h3>Genome Profile of an Athlete</h3>
+    				</a>
+    				<a href="life-help.php" id="life" class="tile active">
+    					<h3>Life Insurance for the Healthy</h3>
+    				</a>
+    				<a href="#" id="emergency" class="tile">
+    					<h3>Emergency Planning</h3>
+    				</a>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+</div>
+
+<?php require_once('footer.html'); ?>
