cf4dd112c50c0cda9a4f49101470dbe5f15888b0
Chris Spurlock <cjs7w6@gmail.com>
2016-10-17 17:43:04 -0500

    Create new Quote - Recommended page to work on mirroring markup from current site

---
+++ new-recommended.php
@@ -0,0 +1,144 @@
+<?php require_once('header.html'); ?>
+
+<form action="" id="comparison-quote-form" name="comparisonQuoteForm" class="background-wrap">
+	<section id="recommended" class="i-know-what-i-need cover main-section" style="">
+	    <div class="overlay"></div>
+	    <div class="container">
+	        <div class="column online">
+	            <h2>Apply Online</h2>
+	            <div class="row">
+	                <div class="best-class">
+	                    <span class="currency">$</span>
+	                    <span class="price">58</span>
+	                    <span class="freq">/mo</span>
+	                </div>
+	                <div id="class_category">
+	                    <span class="rate-name">Preferred Plus Class</span>
+	                </div>
+	                <div class="comparison">
+	                    <h5>Compare to <span class="currency">$</span><span class="price">52</span><span class="freq">/mo</span> Standard Non-Smoker</h5>
+	                </div>
+	                <div class="row recommended-coverage">
+	                    <div class="text-center col-md-6">
+                            <h4>Coverage</h4>
+                            <h3 class="calculated-coverage">$100,000</h3>
+                        </div>
+	                    <div class="text-center col-md-6">
+                            <h4>Term Period</h4>
+                            <h3 class="calculated-term">10 Years</h3>
+                        </div>
+	                </div>
+	                <div id="transamerica_logo">
+	                    <img src="resources/images/icon-transamerica-slider.png" alt="">
+	                </div>
+	                <div id="apply_button">
+	                    <a id="#original-quote-applynow" href="#" class="btn btn-primary apply-now" wpvalue="apply_now">Apply Now</a>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="column call">
+	            <h2>Call To Apply</h2>
+	            <div class="row">
+	                <div class="column">
+	                    <h6>Metlife<sup>TM</sup></h6>
+	                    <p>Coverage: <span class="price">$700,000</span></p>
+	                    <p>Term: <span class="term">10 years</span></p>
+	                    <p>Call <a href="tel:18005491664">1-800-549-1664</a></p>
+	                </div>
+	                <div class="column">
+	                    <div class="multicarrier-class">
+	                        <span class="currency">$</span>
+		                    <span class="price">52</span>
+		                    <span class="freq">/mo</span>
+	                    </div>
+	                    <div class="comparison">
+	                        <h5>Compare to <span class="price">$52</span>/mo</h5>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="row">
+	                <div class="column">
+	                    <h6>Banner Life<sup>TM</sup></h6>
+	                    <p>Coverage: <span class="price">$700,000</span></p>
+	                    <p>Term: <span class="term">10 years</span></p>
+	                    <p>Call <a href="tel:18005491664">1-800-549-1664</a></p>
+	                </div>
+	                <div class="column">
+	                    <div class="price-wrapper">
+	                        <div class="multicarrier-class">
+	                        <span class="currency">$</span>
+		                    <span class="price">56</span>
+		                    <span class="freq">/mo</span>
+	                    </div>
+	                    </div>
+	                    <div class="comparison">
+	                        <h5>Compare to <span class="price">$52</span>/mo</h5>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="row">
+	                <div class="column">
+	                    <h6>AIG Life<sup>TM</sup></h6>
+	                    <p>Coverage: <span class="price">$700,000</span></p>
+	                    <p>Term: <span class="term">10 years</span></p>
+	                    <p>Call <a href="tel:18005491664">1-800-549-1664</a></p>
+	                </div>
+	                <div class="column">
+	                    <div class="price-wrapper">
+	                        <div class="multicarrier-class">
+	                        <span class="currency">$</span>
+		                    <span class="price">61</span>
+		                    <span class="freq">/mo</span>
+	                    </div>
+	                    </div>
+	                    <div class="comparison">
+	                        <h5>Compare to <span class="price">$52</span>/mo</h5>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="row">
+	                <div class="column">
+	                    <h6>Aflac<sup>TM</sup></h6>
+	                    <p>Coverage: <span class="price">$700,000</span></p>
+	                    <p>Term: <span class="term">10 years</span></p>
+	                    <p>Call <a href="tel:18005491664">1-800-549-1664</a></p>
+	                </div>
+	                <div class="column">
+	                    <div class="price-wrapper">
+	                        <div class="multicarrier-class">
+	                        <span class="currency">$</span>
+		                    <span class="price">59</span>
+		                    <span class="freq">/mo</span>
+	                    </div>
+	                    </div>
+	                    <div class="comparison">
+	                        <h5>Compare to <span class="price">$52</span>/mo</h5>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+	<section id="range_sliders">
+	    <div class="sliders">
+	        <span class="slider-label">Amount of Coverage</span>
+	        <span class="quote-coverage calculated-coverage">$300,000</span>
+	        <div id="comparison-coverage" class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all">
+                <div class="ui-slider-range ui-widget-header ui-corner-all ">
+                </div>
+                <span class="ui-slider-handle ui-state-default ui-corner-all upd-cov-slider-range" tabindex="0" style="left: 0%;"></span>
+            </div>
+	    </div>
+	    <div class="sliders">
+	        <span class="slider-label">Number of Years to Maintain Coverage</span>
+	        <span class="quote-term calculated-term">10 Years</span>
+	        <div id="comparison-term" class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all">
+                <div class="ui-slider-range ui-widget-header ui-corner-all ">
+                </div>
+                <span class="ui-slider-handle ui-state-default ui-corner-all upd-term-slider-range" tabindex="0" style="left: 0%;"></span>
+            </div>
+	    </div>
+	</section>
+</form>
+
+<?php require_once('footer.html'); ?>
