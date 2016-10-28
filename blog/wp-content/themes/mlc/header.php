40f0766254b373fa483a764c909228814e18c614
Chris Spurlock <cjs7w6@gmail.com>
2016-10-28 11:08:42 -0500

    Still broken links

--- blog/wp-content/themes/mlc/header.php
+++ blog/wp-content/themes/mlc/header.php
@@ -144,10 +144,10 @@
                                 <a href="https://www.mylifecovered.net/myLifeCovered/misconceptions.do">12 Common Misconceptions</a>
                             </li>
                             <li>
-                                <a href="category/fitness-for-life">Fitness for Life</a>
+                                <a href="/category/fitness-for-life">Fitness for Life</a>
                             </li>
                             <li>
-                                <a href="category/life-prepared">Life Prepared</a>
+                                <a href="/category/life-prepared">Life Prepared</a>
                             </li>
                         </ul>
                         <a href="#" id="open_menu">
