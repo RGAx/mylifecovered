e81af6d9f99e074a576f6d277bac2324e4868e3a
Chris Spurlock <cjs7w6@gmail.com>
2016-11-03 12:01:07 -0500

    Add Google Analytics tag to footer

--- blog/wp-content/themes/mlc/footer.php
+++ blog/wp-content/themes/mlc/footer.php
@@ -47,6 +47,7 @@
 
 <!-- Asynchronous google analytics; this is the official snippet.
          Replace UA-XXXXXX-XX with your site's ID and domainname.com with your domain, then uncomment to enable.
+-->
 
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -54,11 +55,11 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-XXXXXX-XX', 'domainname.com');
+  ga('create', 'UA-79521976-1', 'auto');
   ga('send', 'pageview');
 
 </script>
--->
+
 
 </body>
 
