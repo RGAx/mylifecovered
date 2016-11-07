1d0b50b35d8b96b6ada9558c33fa1123f3f7c403
Chris Spurlock <cjs7w6@gmail.com>
2016-11-07 09:29:59 -0600

    Add new tile on welcome and child level pages, add Everplans landing page

--- everplans.php
+++ everplans.php
@@ -1,29 +1,29 @@
 <?php require_once('header.html'); ?>
 
-<div id="blood" class="background-wrap child-landing">
-	<section id="blood_hero" class="cover text-center">
+<div id="everplans" class="background-wrap child-landing">
+	<section id="everplans_hero" class="cover text-center">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>Not Your Average <br>Blood Test</h1>
-                    <p>You are uniquely you. Make your nutrition and workout programs work for you by understanding your body’s markers and gain strategies to capitalize on them.</p>
-                    <a href="https://www.wellnessfx.com/my-life-covered" class="btn btn-primary">Learn More</a>
+                    <h1>You train to be prepared. <br>Be prepared for the unknown.</h1>
+                    <p>Create a secure, digital archive of everything your loved ones will need should something happen to you.</p>
+                    <a href="https://www.everplans.com/" class="btn btn-primary">Learn More</a>
                 </div>
             </div>
         </div>
     </section>
-    <section id="blood_tiles" class="tiles child-tiles">
+    <section id="everplans_tiles" class="tiles child-tiles">
     	<div class="container">
     		<div class="row">
     			<div class="col-md-12">
-    				<a href="blood.php" id="blood" class="tile active">
+    				<a href="blood.php" id="blood" class="tile">
     					<h3>Athletic <br>Blood Profile</h3>
     				</a>
     				<a href="life-help.php" id="life" class="tile">
     					<h3>Term Life <br>Insurance</h3>
     				</a>
-    				<a href="everplans.php" id="emergency" class="tile">
+    				<a href="everplans.php" id="emergency" class="tile active">
     					<h3>Everplans: Life & <br>Legacy Planning</h3>
     				</a>
     			</div>
