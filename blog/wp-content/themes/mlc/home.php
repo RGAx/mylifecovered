ec3590665f94f8baf3533e65b2e728a4dfa3d57c
Ryan Haider <rchaider@gmail.com>
2016-11-16 14:26:08 -0600

    [HF] Bring featured down into the main loop

--- blog/wp-content/themes/mlc/home.php
+++ blog/wp-content/themes/mlc/home.php
@@ -11,50 +11,54 @@ get_header(); ?>
  	        <div class="col-md-8">
  	        	<?php get_search_form(); ?>
  	        	<?php
- 	        	$featured_posts = get_posts(array("posts_per_page"=>1,"post_type"=>"post", "post_status"=>"publish", "meta_key"=>"featured", "meta_value"=>true));
- 	        	$featured_post = $featured_posts[0];
- 	        	$featured_post_img = wp_get_attachment_image_src( get_post_thumbnail_id( $featured_post ), 'full' );
-
 		       global $wp_query;
                $current_page = max(1, get_query_var('paged'));
                if($current_page == 1) {
  	        	?>
-				<article class="featured-post" id="post-<?=$featured_post->ID; ?>">
-					<img src="<?=$featured_post_img[0];?>">
-					<div class="content-wrapper">
-						<h2 class="title"><a href="<?=get_the_permalink($featured_post->ID) ?>"><?=$featured_post->post_title;?></a></h2>
-						<div class="entry">
-							<?=the_excerpt(); ?>
-						</div>
-						<a class="btn btn-primary" href="<?=get_the_permalink($featured_post->ID) ?>">Read More</a>
-					</div>
-				</article>
 				<?php } ?>
 				<div class="other-articles">
+				<?php $counter = 0; ?>
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<article class="standard-post" id="post-<?php the_ID(); ?>">
-						<h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-						<p class='publish-details'>
- 	        				<?php
- 	        					echo get_the_date();
-								$author = get_field( 'author' );
-								if ($author) {
-							?>
-								Author: <?=$author;?>
-							<?php
-								}
-							?>
- 	        			</p>
-						<div class="entry excerpt">
-							<?php $other_post_img = wp_get_attachment_image_src( get_post_thumbnail_id( ), 'full' ); ?>
-							<?php
-								if ($other_post_img) {
-							?>
-							<img src="<?=$other_post_img[0];?>">
-							<?php } ?>
-							<?=the_excerpt(); ?>
-						</div>
-					</article>
+					<?php
+					++$counter;
+					if($counter == 1) {
+		 	        	$featured_post_img = wp_get_attachment_image_src( get_post_thumbnail_id( $post ), 'full' );
+		 	        	?>
+						<article class="featured-post" id="post-<?=$post->ID; ?>">
+							<img src="<?=$featured_post_img[0];?>">
+							<div class="content-wrapper">
+								<h2 class="title"><a href="<?=get_the_permalink($post->ID) ?>"><?=$post->post_title;?></a></h2>
+								<div class="entry">
+									<?=the_excerpt(); ?>
+								</div>
+								<a class="btn btn-primary" href="<?=get_the_permalink($post->ID) ?>">Read More</a>
+							</div>
+						</article>
+					<?php } else { ?>
+						<article class="standard-post" id="post-<?php the_ID(); ?>">
+							<h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+							<p class='publish-details'>
+	 	        				<?php
+	 	        					echo get_the_date();
+									$author = get_field( 'author' );
+									if ($author) {
+								?>
+									Author: <?=$author;?>
+								<?php
+									}
+								?>
+	 	        			</p>
+							<div class="entry excerpt">
+								<?php $other_post_img = wp_get_attachment_image_src( get_post_thumbnail_id( ), 'full' ); ?>
+								<?php
+									if ($other_post_img) {
+								?>
+								<img src="<?=$other_post_img[0];?>">
+								<?php } ?>
+								<?=the_excerpt(); ?>
+							</div>
+						</article>
+					<?php } ?>
 				<?php endwhile; ?>
 				<?php endif; ?>
 				</div>
